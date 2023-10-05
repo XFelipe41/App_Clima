538968b83522b3738d8c9f8d228e7714a0eb06f1
XFelipe41 <jfcaicedot@ut.edu.co>
2023-10-05 10:17:44 -0500

    Subida del Proyecto

---
+++ src/components/Loading.jsx
@@ -0,0 +1,16 @@
+const Loading = () => {
+  return (
+    <div className="loading">
+      <div className="sk-chase">
+        <div className="sk-chase-dot" />
+        <div className="sk-chase-dot" />
+        <div className="sk-chase-dot" />
+        <div className="sk-chase-dot" />
+        <div className="sk-chase-dot" />
+        <div className="sk-chase-dot" />
+      </div>
+    </div>
+  );
+};
+
+export default Loading;
